5a5d66d51bb011259950202d2ad2a227a2a639a1
Zachary Murphy <murphyz@my.wccc.edu>
2016-02-27 19:02:07 -0500

    help!

--- src/conferencereg/ConferenceReg.java
+++ src/conferencereg/ConferenceReg.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package conferencereg;
 import java.awt.*; 
 import javax.swing.*;
 import java.text.DecimalFormat;
 import java.awt.event.*;
 import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 /**
  *
  * @author zacha
@@ -28,8 +26,9 @@ public class ConferenceReg  extends JFrame{
 
     double total = 0;
     double keynote = 0;
-    double work = 0;
     double regs = 0;
+            double work = 0;
+
 
     
     public ConferenceReg(){
@@ -39,8 +38,9 @@ public class ConferenceReg  extends JFrame{
     setSize(500,500);
     
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setVisible(true);
         buildPanel();
+                setVisible(true);
+
     
     
     }
@@ -55,7 +55,7 @@ private void buildPanel(){
               String [] workshopPos = {"Introduction to E-commerce", "The Future of the Web", "Advanced Java Programming", "Network Security"};
               //double [] workshopCostv= {295, 295, 395, 395};
 
-     JList workshop = new JList (workshopPos);//to make it show on gui
+     JList workshop = new JList(workshopPos);//to make it show on gui
     JList workshopGet = new JList(); //to get info
 
     generalReg.addActionListener(new Radio() );
@@ -63,12 +63,15 @@ private void buildPanel(){
     mainPanel.add(generalReg);
     mainPanel.add(studentReg);
     mainPanel.add(dinnerKeynote);
+    dinnerKeynote.addActionListener(new CheckListener());
     JButton calculate = new JButton("Calculate");
     mainPanel.add(calculate);
     mainPanel.add(workshop);
     mainPanel.add(workshopGet);
+    workshop.addListSelectionListener(new ListListener());
+
     calculate.addActionListener(new CalcListener());
-    
+    //Get.yoga()
     
    
     
@@ -78,38 +81,41 @@ private void buildPanel(){
         
     }
 
-private class Radio implements ActionListener{
+private class  Radio implements ActionListener{
         
     
 
         @Override
-        public void actionPerformed(ActionEvent e) {
+        public void  actionPerformed(ActionEvent e) {
             regs = 0;
-            if(e.getSource()==generalReg){
-                regs += 895.00;
+            if(generalReg.isSelected()){
+                regs = 895.00;
             }else{
-                regs += 495.00;
+                regs = 495.00;
             }
         }
     }
 
-private class ListSelectionListener implements ListSelectionListener{
-  
-    public void values (ListSelectionEvent e){
-        int [] select = workshopGetJlist.getSelectedIndices();
+private class ListListener implements ListSelectionListener{
+    
+    @Override
+    public void valueChanged (ListSelectionEvent e){
+        int [] select = workshop.getSelectedIndices();
         
         work = 0;
         for (int i = 0; i< select.length; i++){
             if(select[i] == 0 || select[i] ==1){
-                work += 295.00;
+                work = 295.00;
             }else if (select[i] == 2 || select [i] ==3){
-                work +=395.00;
+                work =395.00;
             }else{
                 work += 0;
             }
         }
+    } 
+
+
     }
-}
 
 private class CheckListener implements ActionListener{
 
@@ -119,7 +125,7 @@ private class CheckListener implements ActionListener{
                 if (dinnerKeynote.isSelected()){
                     keynote += 30.00;
                 }else{
-                    keynote = 0;
+                    keynote -= 30.00;
                 }
             }
         }
@@ -130,7 +136,7 @@ private class CalcListener implements ActionListener{
         @Override
         public void actionPerformed(ActionEvent e) {
             total = regs + keynote + work;
-        JOptionPane.showMessageDialog("Total" + total);        }
+        JOptionPane.showMessageDialog(null, "Total" + total);        }
     
 }
 
